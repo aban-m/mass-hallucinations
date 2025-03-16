00b29d42adedb51972cad8135058b2e318c7fde1
Aban <aban.salah.mahmoud@gmail.com>
2025-03-16 16:31:26 +0200

    Update auth-options (not working)

--- src/lib/auth/auth-options.ts
+++ src/lib/auth/auth-options.ts
@@ -1,28 +1,55 @@
-import { NextAuthOptions } from "next-auth";
+import { JWT, NextAuthOptions, Session } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { db } from "../db";
-import { user } from "../db/schema";
-import { eq } from "drizzle-orm";
+import { getUserByEmail } from "../db/queries";
+import { AuthError } from "../common";
+import { transmittedUser } from "../../../next-auth";
 
 const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      },
     }),
   ],
+  session: {
+    strategy: "jwt"
+  },
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async session({ session }) {
-      if (session.user) {
-        const userRecord = await db
-          .select()
-          .from(user)
-          .where(eq(user.email, session.user.email!));
-        if (userRecord) {
-          session.user.id = userRecord[0].id;
+    async signIn() {
+      console.log('\t\tSIGNIN CALLBACK')
+      return true;
+    },
+    async jwt({ token, user }) {
+      console.log('\t\tJWT CALLBACK')
+      if (user?.email) {
+        try {
+          const record = await getUserByEmail(user.email!);
+          token.user = record!;
+          console.log("Populated:", token.user);
+        } catch (error) {
+          if (error instanceof AuthError) {
+            return token;
+          } else {
+            throw error;
+          }
         }
       }
-      return session;
+      return token
+    },
+
+    async session({ session, token }) {
+      console.log('\t\tSESSION CALLBACK')
+      console.log("Called session");
+      if (token.user) {
+        session.user = token.user as transmittedUser;
+      }
+      return (async () => session)() 
     },
   },
 };
