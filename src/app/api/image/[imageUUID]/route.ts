1eb71e6555ae64eab77ebb0738daca989aa52613
Aban <aban.salah.mahmoud@gmail.com>
2025-03-19 01:22:31 +0200

    Make the /image endpoint context-aware

--- src/app/api/image/[imageUUID]/route.ts
+++ src/app/api/image/[imageUUID]/route.ts
@@ -1,22 +1,44 @@
+import { canAccess, getCreationByUUID } from "@/lib/db/queries";
 import { fetchImage } from "@/lib/storage";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
+import { z } from "zod";
 
 export async function GET(
     request: Request,
     { params }: { params: { imageUUID?: string } }
 ) {
+
     const { imageUUID } = await params;
-    if (!imageUUID) {
-        return NextResponse.json({ error: "Not Found" }, { status: 404 });
+
+    if (!z.string().uuid().safeParse(imageUUID)) {
+        return NextResponse.json(null, { status: 400 });
+    }
+
+    const creation = await getCreationByUUID(imageUUID!)
+    if (!creation) { 
+        return NextResponse.json(null, {status: 400})
+    }
+
+    const accessible = creation!.isPublic
+        || await (async () => {
+            const session = await getServerSession()
+            return !!(session?.user && canAccess(session.user!, creation))
+        })()
+    
+    if (!accessible) {
+        return NextResponse.json(null, {status: 403})
     }
 
     try {
-        const url = await fetchImage(imageUUID, null)
+        const url = await fetchImage(imageUUID!, null)
         if (!url) {
             return NextResponse.json({ error: "Not Found" }, { status: 404 });
         }
         return NextResponse.redirect(url);
+
     } catch (error) {
+
         return NextResponse.json({ error: "Not Found" }, { status: 404 });
     }
 }
