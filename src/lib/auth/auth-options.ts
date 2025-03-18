83a888989aaef5870eed6cccdfacb637d474ed4c
Aban <aban.salah.mahmoud@gmail.com>
2025-03-19 01:24:32 +0200

    Lint

--- src/lib/auth/auth-options.ts
+++ src/lib/auth/auth-options.ts
@@ -10,26 +10,26 @@ const authOptions: NextAuthOptions = {
     }),
   ],
   session: {
-    strategy: "jwt"
+    strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async signIn() {
-      console.log('\t\tSIGNIN CALLBACK')
+      console.log("\t\tSIGNIN CALLBACK");
       return true;
     },
     async jwt({ token, user }) {
-      console.log('\t\tJWT CALLBACK')
-      return token
+      console.log("\t\tJWT CALLBACK");
+      return token;
     },
 
     async session({ session, token }) {
-      console.log('\t\tSESSION CALLBACK')
-      if (token.email && !session.user.id) {
-      session.user = await getUserByEmail(token.email)
-      console.log('\t\t\tDB TRIP!')
+      console.log("\t\tSESSION CALLBACK");
+      if (token.email && !session.user?.id) {
+        session.user = await getUserByEmail(token.email);
+        console.log("\t\t\tDB TRIP!");
       }
-      return session
+      return session;
     },
   },
 };
