e4619cad271587fcdb3ebbd4ab933ce3649c9712
Aban <aban.salah.mahmoud@gmail.com>
2025-03-19 03:24:17 +0200

    Apply cosmetic change to CreationView

--- src/components/CreationView.tsx
+++ src/components/CreationView.tsx
@@ -1,17 +1,27 @@
-import Image, {ImageProps} from "next/image";
-import { creation } from "@/lib/db/schema"
+import Image, { ImageProps } from "next/image";
+import { creation } from "@/lib/db/schema";
+import Link from "next/link";
 
-type CreationViewProps = Omit<ImageProps & (typeof creation.$inferSelect), "src" | "alt">
+type CreationViewProps = Omit<
+  ImageProps & typeof creation.$inferSelect,
+  "src" | "alt"
+>;
 
 export default function CreationView(props: CreationViewProps) {
-    return (
-    <div><Image 
-        src={`/api/image/${props.id}`}
-        alt={props.title}
-        unoptimized
-        width={360} height={480}
-    />
-    <p>{props.title}</p>
-    <i>Prompt: {props.prompt}</i>
-    </div>)
+  return (
+    <div>
+      <Link href={`/piece/${props.id}`}>
+        <Image
+          src={`/api/image/${props.id}`}
+          alt={props.title}
+          unoptimized
+          width={360}
+          height={480}
+        />
+      </Link>
+      <pre>{props.id}</pre>
+      <p>{props.title}</p>
+      <i>Prompt: {props.prompt}</i>
+    </div>
+  );
 }
