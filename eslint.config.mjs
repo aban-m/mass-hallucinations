d9792909a25b2ad82cbccb3ca3e20039f19e2390
Aban <aban.salah.mahmoud@gmail.com>
2025-03-20 21:57:23 +0200

    Add more eslint packages/config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import unusedImports from "eslint-plugin-unused-imports";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {plugins: { "unused-imports": unusedImports },
+  rules: {
+    "no-unused-vars": ["error", { vars: "all", args: "after-used" }],
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      { vars: "all", varsIgnorePattern: "^_" }
+    ]
+  }},
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
